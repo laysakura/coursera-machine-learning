c2b9049a120ff1246650f9cfcb8871310f476202
nakatani.sho <nakatani.sho@dena.jp>
2015-08-14 18:08:35 +0900

    cost function w/ regularization

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,9 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+grad = (1/m) .* sum(sigmoid(X * theta) - y) + (lambda / m) .* theta;
 
-
-
-
+J = (1/m) .* sum(-y .* log(sigmoid(X * theta)) - (1 - y) .* (log(1 .- sigmoid(X * theta)))) + (lambda / (2 * m)) .* sum(theta(2:length(theta)) .^ 2);
 
 % =============================================================
 
