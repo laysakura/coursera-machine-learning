1b2bc8c2b291f6f6b949331861b1994d1c472694
Sho Nakatani <lay.sakura@gmail.com>
2015-08-15 02:45:57 +0900

    Regularized Logistic Regression

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,13 +36,11 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+theta_ = [0; theta(2:length(theta))];
 
+grad = (1/m) .* X' * (sigmoid(X * theta) - y) + (lambda / m) .* theta_;
 
-
-
-
-
-
+J = (1/m) .* sum(-y .* log(sigmoid(X * theta)) - (1 - y) .* (log(1 .- sigmoid(X * theta)))) + (lambda / (2 * m)) .* sum(theta(2:length(theta)) .^ 2);
 
 
 % =============================================================
